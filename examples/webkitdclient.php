96cb7b571dc4beb15ba17f29d6400b45731e003b
The Pickling Jar Limited <github@thepicklingjar.com>
2010-08-22 19:08:12 +0100

    added xvfb headless script, php webkitd client and first webkitd implementation

---
+++ examples/webkitdclient.php
@@ -0,0 +1,390 @@
+<?php
+/*
+ * Copyright (c) 2010 The Pickling Jar Ltd <code@thepicklingjar.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+define('CMDURL', 1);
+define('CMDGET', 2);
+define('CMDPOST', 3);
+define('CMDSETPOSTVAL', 4);
+define('CMDSETCOOKIE', 5);
+define('CMDDELCOOKIE', 6);
+define('CMDRETURNCOOKIE', 7);
+define('CMDSETPROXY', 8);
+define('CMDRETURNPROXY', 9);
+define('CMDSETREFERRER', 10);
+define('CMDSETUSERAGENT', 11);
+define('CMDNEWCOOKIEJAR', 12);
+define('CMDEXECUTE', 13);
+define('CMDQUIT', 14);
+define('CMDRETURNHTML', 15);
+define('CMDRUNJS', 16);
+define('CMDSSLERRORSOFF', 17);
+define('CMDSSLERRORSON', 18);
+define('CMDHTACCESSUSERNAME', 19);
+define('CMDHTACCESSPASSWORD', 20);
+define('CMDRETURNURL', 21);
+define('CMDRETURNHEADER', 22); /* last request header, could be an img  :/ */
+define('CMDRETURNHTMLSOUP', 23);
+define('CMDRETURNIMAGE', 24);
+define('CMDRUNJQUERY', 25);
+define('CMDINPUTFILL', 26);
+define('CMDINPUTCHECK', 27);
+define('CMDINPUTUNCHECK', 28);
+define('CMDINPUTCHOOSE', 29);
+define('CMDINPUTSELECT', 30);
+define('CMDFORMSUBMIT', 31);
+define('CMDSTAT', 99);
+define('CMDHELP', 0);
+
+$globalerrcode = null;
+$globalerrstr = null;
+
+$fd = webkitd_connect();
+if($fd == false){
+	die('Error: webkitd couldn\'t connect'."\n");
+}
+
+//$res = webkitd_help($fd);
+//echo $res;
+
+
+$res = webkitd_url($fd, 'http://www.thepicklingjar.com/code/webkitd/test302.php');
+if($res == false){
+	webkitd_close($fd);
+	die('Error: webkitd couldn\'t set url'."\n");
+}
+
+$res = webkitd_setuseragent($fd, 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)');
+if($res == false){
+	webkitd_close($fd);
+	die('Error: webkitd couldn\'t set user agent'."\n");
+}
+
+$res = webkitd_setproxy($fd, 'http://127.0.0.1:8080');
+if($res == false){
+	webkitd_close($fd);
+	die('Error: webkitd couldn\'t set proxy'."\n");
+}
+$proxy = webkitd_returnproxy($fd);
+
+$res = webkitd_setproxy($fd, '');
+if($res == false){
+	webkitd_close($fd);
+	die('Error: webkitd couldn\'t set proxy'."\n");
+}
+
+$res = webkitd_setreferrer($fd,'http://www.testreferrer.com');
+if($res == false){
+	webkitd_close($fd);
+	die('Error: webkitd couldn\'t set referrer'."\n");
+}
+
+//set get request
+$res = webkitd_get($fd);
+if($res == false){
+	webkitd_close($fd);
+	die('Error: webkitd couldn\'t set http get'."\n");
+}
+
+//run
+$res = webkitd_execute($fd);
+if($res == false){
+	echo ('Error: webkitd couldn\'t execute - errcode: '.$globalerrcode.' errstr: '.$globalerrstr."\n");
+}
+
+/*$res = webkitd_post($fd);
+//echo "post: ".$res."\n";
+if($res == false){
+	webkitd_close($fd);
+	die('Error: webkitd couldn\'t set http post'."\n");
+}
+
+$res = webkitd_execute($fd);
+if($res == false){
+	echo ('Error: webkitd couldn\'t execute - errcode: '.$globalerrcode.' errstr: '.$globalerrstr."\n");
+}*/
+
+$url = webkitd_returnurl($fd);
+if($url == false){
+	webkitd_close($fd);
+	die('Error: webkitd couldn\'t return url'."\n");
+}
+echo "url: ".$url."\n";
+
+
+/*
+//broke as returns last header (i.e. could be an image)
+$header= webkitd_returnheader($fd);
+if($header == false){
+	webkitd_close($fd);
+	die('Error: webkitd couldn\'t return header'."\n");
+}
+echo "header: ".$header."\n";
+*/
+
+$res = webkitd_runjs($fd, "_jQuery('#test').html('test text js1');");
+#$res = webkitd_runjs($fd, "document.getElementById('test').innerHTML = 'jsrun1';");
+
+//returns tag soup
+$html = webkitd_gethtml($fd);
+if($html == false){
+	webkitd_close($fd);
+	die('Error: webkitd couldn\'t get html'."\n");
+}
+echo "HTML: ".$html."\n";
+
+$regex = '';
+$imgdata = webkitd_returnimage($fd, $regex);
+if($imgdata == false){
+	webkitd_close($fd);
+	die('Error: webkitd couldn\'t return image'."\n");
+}
+else {
+	$ifd = fopen('./img.jpg','w');
+	if($ifd){ 
+		fwrite($ifd,$imgdata);
+		fclose($ifd);
+		echo ('wrote image'."\n");
+	}
+	else {
+		echo ('didn\'t write image'."\n");
+	}
+}
+
+//return url might not be the one we set due to 301s/302s etc
+$url = webkitd_returnurl($fd);
+if($url == false){
+	webkitd_close($fd);
+	die('Error: webkitd couldn\'t return url'."\n");
+}
+echo "final url: ".$url."\n";
+
+$res = webkitd_runjquery($fd, "_jQuery('#test').html('test text js 2');");
+$html = webkitd_gethtml($fd);
+if($html == false){
+	webkitd_close($fd);
+	die('Error: webkitd couldn\'t get html'."\n");
+}
+echo "HTML: ".$html."\n";
+
+/*
+$res = webkitd_inputfill("input[name=enit]", "hola");
+$res = webkitd_inputcheck("input[name=enit]", "hola");
+$res = webkitd_inputuncheck("input[name=enit]", "hola");
+$res = webkitd_inputchoose("input[name=enit]", "hola");
+$res = webkitd_inputselect("input[name=enit]", "hola");
+$res = webkitd_formsubmit("input[name=enit]", "hola");
+*/
+
+
+$res = webkitd_close($fd);
+if($res == false){
+	die('Error: webkitd couldn\'t close'."\n");
+}
+
+
+
+
+
+function webkitd_connect(){
+	$webkitdip = '127.0.0.1';
+	$webkitdport = '3817';
+	$fd = fsockopen($webkitdip, $webkitdport, $errno, $errstr, 10);
+	return $fd;
+}
+
+//0
+function webkitd_help($fd){
+	fwrite($fd, CMDHELP."\n");
+	$str = '';
+	$len = fgets($fd, 1024);
+	$tlen = 0;
+	while($tlen < $len) {
+		$fragment = fread($fd, $len);
+		$str .= $fragment;
+		$tlen += strlen($fragment);
+	}
+	return $str;
+}
+
+//1
+function webkitd_url($fd, $url){
+	fwrite($fd, CMDURL." ".$url."\n");
+	$str = fgets($fd, 1024);
+	if(trim($str) == 'ok'){
+		return true;
+	}
+	return false;
+}
+
+//2
+function webkitd_get($fd){
+	fwrite($fd, CMDGET."\n");
+	$str = fgets($fd, 1024);
+	if(trim($str) == 'ok'){
+		return true;
+	}
+	return false;
+}
+
+//3
+function webkitd_post($fd){
+	fwrite($fd, CMDPOST."\n");
+	$str = fgets($fd, 1024);
+	if(trim($str) == 'ok'){
+		return true;
+	}
+	return false;
+}
+
+//4
+function webkitd_setpostval($fd,$key,$val){
+	fwrite($fd, CMDSETPOSTVAL." ".$key." ".$val."\n");
+	$str = fgets($fd, 1024);
+	if(trim($str) == 'ok'){
+		return true;
+	}
+	return false;
+}
+
+// 8
+function webkitd_setproxy($fd,$proxy){
+	fwrite($fd, CMDSETPROXY." ".$proxy."\n");
+	$str = fgets($fd, 1024);
+	return $str;
+}
+
+//9
+function webkitd_returnproxy($fd){
+	fwrite($fd, CMDRETURNPROXY."\n");
+	$str = fgets($fd, 1024);
+	return $str;
+}
+
+//10
+function webkitd_setreferrer($fd, $ref){
+	fwrite($fd, CMDSETREFERRER." ".$ref."\n");
+	$str = fgets($fd, 1024);
+	if(trim($str) == 'ok'){
+		return true;
+	}
+	return false;
+}
+
+//11
+function webkitd_setuseragent($fd, $ua){
+	fwrite($fd, CMDSETUSERAGENT." ".$ua."\n");
+	$str = fgets($fd, 1024);
+	if(trim($str) == 'ok'){
+		return true;
+	}
+	return false;
+}
+
+//13
+function webkitd_execute($fd){
+	global $globalerrcode;
+	global $globalerrstr;
+	fwrite($fd, CMDEXECUTE."\n");
+	$str = fgets($fd, 1024);
+	if(trim($str) == 'ok'){
+		return true;
+	}
+	$p = explode(" ",$str,3);
+	$globalerrcode = $p[1];
+	$globalerrstr = $p[2];
+	return false;
+}
+
+//14
+function webkitd_close($fd){
+	fwrite($fd, CMDQUIT."\n");
+	$str = fgets($fd, 1024);
+	if(trim($str) == 'ok'){
+		fclose($fd);
+		return true;
+	}
+	fclose($fd);
+	return false;
+}
+
+//15
+function webkitd_gethtml($fd){
+	fwrite($fd, CMDRETURNHTMLSOUP."\n");
+	$html = '';
+	$len = fgets($fd, 1024);
+	$tlen = 0;
+	while($tlen < $len) {
+		$fragment = fread($fd, $len);
+		$html .= $fragment;
+		$tlen += strlen($fragment);
+	}
+	return $html;
+}
+
+//16
+function webkitd_runjs($fd, $js){
+	fwrite($fd, CMDRUNJS." ".$js."\n");
+	$str = fgets($fd, 1024);
+	return $str;
+}
+
+//21
+function webkitd_returnurl($fd){
+	fwrite($fd, CMDRETURNURL."\n");
+	$str = fgets($fd, 1024);
+	return $str;
+}
+
+//22
+function webkitd_returnheader($fd){
+	fwrite($fd, CMDRETURNHEADER."\n");
+	$header = '';
+	$len = fgets($fd, 1024);
+	$tlen = 0;
+	while($tlen < $len) {
+		$fragment = fread($fd, $len);
+		$header .= $fragment;
+		$tlen += strlen($fragment);
+	}
+	return $header;
+}
+
+//24
+function webkitd_returnimage($fd,$regex){
+	fwrite($fd, CMDRETURNIMAGE." ".$regex."\n");
+	$img = '';
+	$len = fgets($fd, 1024);
+	//echo "imagelen ".$len;
+	$tlen = 0;
+	while($tlen < $len) {
+		//echo "tlen: ".$tlen;
+		//echo "len: ".$len;
+		$fragment = fread($fd, $len);
+		//echo $fragment;
+		$img .= $fragment;
+		$tlen += strlen($fragment);
+	}
+	return $img;
+}
+
+//25
+function webkitd_runjquery($fd, $js){
+	fwrite($fd, CMDRUNJQUERY." ".$js."\n");
+	$str = fgets($fd, 1024);
+	return $str;
+}
+?>
---
+++ webkitd.py
@@ -0,0 +1,317 @@
+#Webkitd - A headless webkit daemon written in python
+"""
+Copyright (C) 2010 The Pickling Jar Limited
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import spynner
+import pyquery
+import SocketServer
+import threading
+import sys
+import tempfile
+from PyQt4.QtNetwork import QNetworkRequest
+from PyQt4.QtCore import QUrl
+
+class Webkitd(SocketServer.BaseRequestHandler):
+	quit = 0
+	url = None
+	httpmethod = 'GET'
+	def setup(self):
+		Webkitd.url = None
+		Webkitd.quit = 0
+		Webkitd.browser = spynner.Browser(None, 3)
+		Webkitd.browser.set_html_parser(pyquery.PyQuery)
+		Webkitd.browser.user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.7) Gecko/20100701 Firefox/3.6.7'
+		Webkitd.browser.referrer = None
+
+	def find_key(self, dic, val):
+		return [k for k, v in dic.iteritems() if v == val][0]
+
+	def cmdurl(self, cmd):
+		Webkitd.url = cmd
+		self.request.send('ok\n')
+		print "%s << ok" % self.client_address[0]
+	
+	def cmdget(self,cmd):
+		Webkitd.httpmethod = Webkitd.find_key(self,Webkitd.browser._operation_names, 'get')
+		self.request.send('ok\n')
+		print "%s << ok" % self.client_address[0]
+
+	def cmdpost(self,cmd):
+		Webkitd.httpmethod = Webkitd.find_key(self,Webkitd.browser._operation_names, 'post')
+		self.request.send('ok\n')
+		print "%s << ok" % self.client_address[0]
+	
+	def cmdsetpostval(self,cmd):
+		self.request.send('TODO\n')
+		print "%s << TODO" % self.client_address[0]
+	
+	def cmdsetcookie(self,cmd):
+		Webkitd.browser.set_cookies(self, cmd)
+		self.request.send('ok\n')
+		print "%s << ok" % self.client_address[0]
+	
+	def cmddelcookie(self,cmd):
+		self.request.send('TODO\n')
+		print "%s << TODO" % self.client_address[0]
+
+	def cmdreturncookie(self,cmd):
+		self.request.send(Webkitd.browser.get_cookies(self))
+		print "%s << %s " % (self.client_address[0], Webkitd.browser.get_cookies(self))
+	
+	def cmdsetproxy(self,cmd):
+		proxy = Webkitd.browser.set_proxy(cmd)
+		Webkitd.cmdreturnproxy(self,cmd)
+	
+	def cmdreturnproxy(self,cmd):
+		proxystruct = Webkitd.browser.manager.proxy()
+		proxytype = proxystruct.type()
+		proxy = '';
+
+		if proxytype == 2 :
+			proxy = '';
+		elif proxytype == 1 :
+			if proxystruct.user() == None or proxystruct.user() == '':
+				proxy = "socks://%s:%d" % (proxystruct.hostName(), proxystruct.port())
+			else :
+				proxy = "http://%s:%s@%s:%d" % (proxystruct.user(), proxystruct.password(), proxystruct.hostName(), proxystruct.port())
+				
+		elif proxytype == 3 :
+			if proxystruct.user() == None or proxystruct.user() == '':
+				proxy = "http://%s:%d" % (proxystruct.hostName(), proxystruct.port())
+			else :
+				proxy = "http://%s:%s@%s:%d" % (proxystruct.user(), proxystruct.password(), proxystruct.hostName(), proxystruct.port())
+
+		self.request.send("%s\n" % proxy)
+		print "%s << %s" % (self.client_address[0], proxy)
+	
+	def cmdsetreferrer(self,cmd):
+		Webkitd.browser.referrer = cmd
+		self.request.send('ok\n')
+		print "%s << ok" % self.client_address[0]
+	
+	def cmdsetuseragent(self, cmd):
+		Webkitd.browser.user_agent = cmd
+		self.request.send('ok\n')
+		print "%s << ok" % self.client_address[0]
+	
+	def cmdnewcookiejar(self,cmd):
+		self.request.send('TODO\n')
+		print "%s << TODO" % self.client_address[0]
+	
+	def cmdexecute(self,cmd):
+		req = QNetworkRequest()
+		#req = Webkitd.browser.manager.QWebNetworkRequest()
+		req.setUrl(QUrl(Webkitd.url))
+		req.setRawHeader("Accept-Language","en-us,en;q=0.5");
+		req.setRawHeader("Accept-Charset","ISO-8859-1,utf-8;q=0.7,*;q=0.7");
+		req.setRawHeader("Keep-Alive","115");
+		req.setRawHeader("Connection","keep-alive");
+		if(Webkitd.browser.referrer != None and Webkitd.browser.referrer != ''):
+			req.setRawHeader("Referer",Webkitd.browser.referrer);
+		Webkitd.browser.load_request(req)
+		if Webkitd.browser._reply_status == False:
+			self.request.send('Error %d %s\n' % (Webkitd.browser._errorCode, Webkitd.browser._errorMessage))
+			print "%s << Error %d %s" % (self.client_address[0],Webkitd.browser._errorCode, Webkitd.browser._errorMessage)
+		else :
+			self.request.send('ok\n')
+			print "%s << ok" % self.client_address[0]
+	
+	def cmdquit(self,cmd):
+		Webkitd.quit = 1;
+		self.request.send('ok\n')
+		print "%s << ok" % self.client_address[0]
+	
+	def cmdreturnhtml(self,cmd):
+		print Webkitd.browser.html
+		self.request.send('%s' % Webkitd.browser.html)
+	
+	def cmdreturnhtmlsoup(self,cmd):
+		#soup len = 1 (object), so print to var and get len of that
+		#Lame
+		#print Webkitd.browser.html
+		#Webkitd.browser.set_html_parser(pyquery.PyQuery)
+		Webkitd.browser.soup = Webkitd.browser._get_soup()
+		Webkitd.browser.soup = Webkitd.browser.soup.make_links_absolute(base_url=Webkitd.browser.url)
+		Webkitd.browser.soup = Webkitd.browser.soup.make_img_urls_absolute(base_url=Webkitd.browser.url)
+		soup = "%s" % Webkitd.browser.soup
+		length = len(soup);
+		self.request.send('%d\n' % length)
+		self.request.sendall('%s' % soup)
+		print "%s << %s" % (self.client_address[0],soup)
+	
+	def cmdrunjs(self,cmd):
+		#_runjs_on_jquery
+		Webkitd.browser.runjs(cmd);
+		self.request.send('ok\n')
+		print "%s << ok" % self.client_address[0]
+
+	def cmdrunjquery(self,cmd):
+		#_runjs_on_jquery
+		Webkitd.browser._runjs_on_jquery('USERINPUT', cmd);
+		self.request.send('ok\n')
+		print "%s << ok" % self.client_address[0]
+	
+	def cmdsslerrorsoff(self,cmd):
+		Webkitd.browser.ignore_ssl_errors = True
+		self.request.send('ok\n')
+		print "%s << ok" % self.client_address[0]
+	
+	def cmdsslerrorson(self,cmd):
+		Webkitd.browser.ignore_ssl_errors = False
+		self.request.send('ok\n')
+		print "%s << ok" % self.client_address[0]
+
+	def cmdhtaccessusername(self,cmd):
+		self.request.send('TODO\n')
+		print "%s << TODO" % self.client_address[0]
+
+	def cmdhtaccesspassword(self,cmd):
+		self.request.send('TODO\n')
+		print "%s << TODO" % self.client_address[0]
+
+	def cmdreturnurl(self,cmd):
+		self.request.send('%s\n' % Webkitd.browser.url)
+		print "%s << %s" % (self.client_address[0],Webkitd.browser.url)
+
+	def cmdreturnheader(self,cmd):
+		#Lame
+		header = "Status: %d\n%s" % (Webkitd.browser._httpstatuscode, Webkitd.browser._header)
+		length = len(header);
+		self.request.send('%d\n' % length)
+		self.request.sendall('%s' % header)
+		print "%s << %s" % (self.client_address[0],header)
+
+	def cmdreturnimage(self,imgpath):
+		Webkitd.browser.soup = Webkitd.browser.soup.make_links_absolute(base_url=Webkitd.browser.url)
+		Webkitd.browser.soup = Webkitd.browser.soup.make_img_urls_absolute(base_url=Webkitd.browser.url)
+		tf = tempfile.TemporaryFile()
+		print tf
+		length = Webkitd.browser.download(Webkitd.browser.soup("img:first").attr('src'), tf)
+		self.request.send('%d\n' % length)
+		print "%s << %d img byte count" % (self.client_address[0],length)
+		tf.seek(0,0)
+		self.request.sendall('%s' % tf.read(length))
+		print "%s << IMG ACTUAL BYTES" % self.client_address[0]
+
+        def cmdinputfill(self,selector,value):
+		self.request.send('todo\n')
+		print "%s << todo" % self.client_address[0]
+		
+        def cmdinputcheck(self,selector,value):
+		self.request.send('todo\n')
+		print "%s << todo" % self.client_address[0]
+
+        def cmdinputuncheck(self,selector,value):
+		self.request.send('todo\n')
+		print "%s << todo" % self.client_address[0]
+
+        def cmdinputchoose(self,selector,value):
+		self.request.send('todo\n')
+		print "%s << todo" % self.client_address[0]
+
+        def cmdinputselect(self,selector,value):
+		self.request.send('todo\n')
+		print "%s << todo" % self.client_address[0]
+
+        def cmdformsubmit(self,selector,value):
+		self.request.send('todo\n')
+		print "%s << todo" % self.client_address[0]
+
+	def cmdstat(self,cmd):
+		self.request.send('ok\n')
+		print "%s << ok" % self.client_address[0]
+
+	def cmdhelp(self,cmd):
+		cmds = "%s\n" % Webkitd.cmds
+		length = len(cmds);
+		self.request.send('%d\n' % length)
+		self.request.sendall('%s' % cmds)
+		print "%s << %s" % (self.client_address[0],cmds)
+
+	cmds = {
+		1 : cmdurl,
+		2 : cmdget,
+		3 : cmdpost,
+		4 : cmdsetpostval,
+		5 : cmdsetcookie,
+		6 : cmddelcookie,
+		7 : cmdreturncookie,
+		8 : cmdsetproxy,
+		9 : cmdreturnproxy,
+		10 : cmdsetreferrer,
+		11 : cmdsetuseragent,
+		12 : cmdnewcookiejar,
+		13 : cmdexecute,
+		14 : cmdquit,
+		15 : cmdreturnhtml,
+		16 : cmdrunjs,
+		17 : cmdsslerrorsoff,
+		18 : cmdsslerrorson,
+		19 : cmdhtaccessusername,
+		20 : cmdhtaccesspassword,
+		21 : cmdreturnurl,
+		22 : cmdreturnheader,
+		23 : cmdreturnhtmlsoup,
+		24 : cmdreturnimage,
+		25 : cmdrunjquery,
+		26 : cmdinputfill,
+		27 : cmdinputcheck,
+		28 : cmdinputuncheck,
+		29 : cmdinputchoose,
+		30 : cmdinputselect,
+		31 : cmdformsubmit,
+		99 : cmdstat,
+		0 : cmdhelp
+	}
+
+	def handle(self):
+		print >> sys.stderr, "%s Connected" % self.client_address[0]
+
+		Webkitd.httpmethod = Webkitd.find_key(self,Webkitd.browser._operation_names, 'get')
+
+		# self.request is the TCP socket connected to the client
+		while Webkitd.quit == 0:
+			self.data = self.request.recv(4096).strip()
+			command = self.data.partition(" ")
+			cmd = int(command[0])
+			print "%s >> Command %d (%s): %s" % (self.client_address[0], cmd,Webkitd.cmds[cmd], command[2])
+		
+			Webkitd.cmds[cmd](self,command[2])
+
+		print >> sys.stderr, "%s Disconnected" % self.client_address[0]
+		Webkitd.browser.close()
+		
+		#browser.runjs("console.log('I can run jQuery: ' + jQuery('a:first').attr('href'))")
+		#browser.select("#esen")
+		#browser.fill("input[name=enit]", "hola")
+		#browser.click("input[name=b]")
+		#browser.wait_page_load()
+
+class ForkingServer(SocketServer.ForkingMixIn,SocketServer.TCPServer):
+	pass
+
+if __name__ == "__main__":
+	HOST, PORT = "localhost", 3817
+	SocketServer.TCPServer.allow_reuse_address = 1 
+	#server = SocketServer.TCPServer((HOST, PORT), Webkitd)
+	server = ForkingServer((HOST, PORT), Webkitd)
+	t = threading.Thread(target=server.serve_forever)
+	t.setDaemon(True) # don't hang on exit
+	t.start()
+
+	print >> sys.stderr, 'WebkitD server started: waiting for connections...'
+	server.serve_forever()
