b7de54de9b25b39045072a5cd0738e9b4907cac3
The Pickling Jar Limited <github@thepicklingjar.com>
2010-08-23 01:04:19 +0100

    Shuffled code around in webkitdclient.php, updated readme

--- examples/webkitdclient.php
+++ examples/webkitdclient.php
@@ -15,6 +15,8 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+
+//defines for commands from webkitd.py
 define('CMDURL', 1);
 define('CMDGET', 2);
 define('CMDPOST', 3);
@@ -52,156 +54,9 @@ define('CMDHELP', 0);
 $globalerrcode = null;
 $globalerrstr = null;
 
-$fd = webkitd_connect();
-if($fd == false){
-	die('Error: webkitd couldn\'t connect'."\n");
-}
+run_test();
 
-//$res = webkitd_help($fd);
-//echo $res;
-
-
-$res = webkitd_url($fd, 'http://www.thepicklingjar.com/code/webkitd/test302.php');
-if($res == false){
-	webkitd_close($fd);
-	die('Error: webkitd couldn\'t set url'."\n");
-}
-
-$res = webkitd_setuseragent($fd, 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)');
-if($res == false){
-	webkitd_close($fd);
-	die('Error: webkitd couldn\'t set user agent'."\n");
-}
-
-$res = webkitd_setproxy($fd, 'http://127.0.0.1:8080');
-if($res == false){
-	webkitd_close($fd);
-	die('Error: webkitd couldn\'t set proxy'."\n");
-}
-$proxy = webkitd_returnproxy($fd);
-
-$res = webkitd_setproxy($fd, '');
-if($res == false){
-	webkitd_close($fd);
-	die('Error: webkitd couldn\'t set proxy'."\n");
-}
-
-$res = webkitd_setreferrer($fd,'http://www.testreferrer.com');
-if($res == false){
-	webkitd_close($fd);
-	die('Error: webkitd couldn\'t set referrer'."\n");
-}
-
-//set get request
-$res = webkitd_get($fd);
-if($res == false){
-	webkitd_close($fd);
-	die('Error: webkitd couldn\'t set http get'."\n");
-}
-
-//run
-$res = webkitd_execute($fd);
-if($res == false){
-	echo ('Error: webkitd couldn\'t execute - errcode: '.$globalerrcode.' errstr: '.$globalerrstr."\n");
-}
-
-/*$res = webkitd_post($fd);
-//echo "post: ".$res."\n";
-if($res == false){
-	webkitd_close($fd);
-	die('Error: webkitd couldn\'t set http post'."\n");
-}
-
-$res = webkitd_execute($fd);
-if($res == false){
-	echo ('Error: webkitd couldn\'t execute - errcode: '.$globalerrcode.' errstr: '.$globalerrstr."\n");
-}*/
-
-$url = webkitd_returnurl($fd);
-if($url == false){
-	webkitd_close($fd);
-	die('Error: webkitd couldn\'t return url'."\n");
-}
-echo "url: ".$url."\n";
-
-
-/*
-//broke as returns last header (i.e. could be an image)
-$header= webkitd_returnheader($fd);
-if($header == false){
-	webkitd_close($fd);
-	die('Error: webkitd couldn\'t return header'."\n");
-}
-echo "header: ".$header."\n";
-*/
-
-$res = webkitd_runjs($fd, "_jQuery('#test').html('test text js1');");
-#$res = webkitd_runjs($fd, "document.getElementById('test').innerHTML = 'jsrun1';");
-
-//returns tag soup
-$html = webkitd_gethtml($fd);
-if($html == false){
-	webkitd_close($fd);
-	die('Error: webkitd couldn\'t get html'."\n");
-}
-echo "HTML: ".$html."\n";
-
-$regex = '';
-$imgdata = webkitd_returnimage($fd, $regex);
-if($imgdata == false){
-	webkitd_close($fd);
-	die('Error: webkitd couldn\'t return image'."\n");
-}
-else {
-	$ifd = fopen('./img.jpg','w');
-	if($ifd){ 
-		fwrite($ifd,$imgdata);
-		fclose($ifd);
-		echo ('wrote image'."\n");
-	}
-	else {
-		echo ('didn\'t write image'."\n");
-	}
-}
-
-//return url might not be the one we set due to 301s/302s etc
-$url = webkitd_returnurl($fd);
-if($url == false){
-	webkitd_close($fd);
-	die('Error: webkitd couldn\'t return url'."\n");
-}
-echo "final url: ".$url."\n";
-
-$res = webkitd_runjquery($fd, "_jQuery('#test').html('test text js 2');");
-$html = webkitd_gethtml($fd);
-if($html == false){
-	webkitd_close($fd);
-	die('Error: webkitd couldn\'t get html'."\n");
-}
-echo "HTML: ".$html."\n";
-
-/*
-$res = webkitd_inputfill("input[name=enit]", "hola");
-$res = webkitd_inputcheck("input[name=enit]", "hola");
-$res = webkitd_inputuncheck("input[name=enit]", "hola");
-$res = webkitd_inputchoose("input[name=enit]", "hola");
-$res = webkitd_inputselect("input[name=enit]", "hola");
-$res = webkitd_formsubmit("input[name=enit]", "hola");
-*/
-
-
-$res = webkitd_close($fd);
-if($res == false){
-	die('Error: webkitd couldn\'t close'."\n");
-}
-
-
-
-
-
-function webkitd_connect(){
-	$webkitdip = '127.0.0.1';
-	$webkitdport = '3817';
+function webkitd_connect($webkitdip = '127.0.0.1', $webkitdport = 3817){
 	$fd = fsockopen($webkitdip, $webkitdport, $errno, $errstr, 10);
 	return $fd;
 }
@@ -386,5 +241,139 @@ function webkitd_runjquery($fd, $js){
 	fwrite($fd, CMDRUNJQUERY." ".$js."\n");
 	$str = fgets($fd, 1024);
 	return $str;
+}
+
+
+function run_test(){
+	//connect to webkitd
+	$fd = webkitd_connect('127.0.0.1', 3817);
+	if($fd == false){
+		die('Error: webkitd couldn\'t connect'."\n");
+	}
+
+	//$res = webkitd_help($fd);
+	//echo $res;
+
+
+	//set url
+	$res = webkitd_url($fd, 'http://www.thepicklingjar.com/code/webkitd/test302.php');
+	if($res == false){
+		webkitd_close($fd);
+		die('Error: webkitd couldn\'t set url'."\n");
+	}
+
+	//set useragent
+	$res = webkitd_setuseragent($fd, 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)');
+	if($res == false){
+		webkitd_close($fd);
+		die('Error: webkitd couldn\'t set user agent'."\n");
+	}
+
+	//set proxy
+	$res = webkitd_setproxy($fd, 'http://127.0.0.1:8080');
+	if($res == false){
+		webkitd_close($fd);
+		die('Error: webkitd couldn\'t set proxy'."\n");
+	}
+	//get current proxy
+	$proxy = webkitd_returnproxy($fd);
+
+	//reset proxy
+	$res = webkitd_setproxy($fd, '');
+	if($res == false){
+		webkitd_close($fd);
+		die('Error: webkitd couldn\'t set proxy'."\n");
+	}
+
+	//set referrer
+	$res = webkitd_setreferrer($fd,'http://www.testreferrer.com');
+	if($res == false){
+		webkitd_close($fd);
+		die('Error: webkitd couldn\'t set referrer'."\n");
+	}
+
+	//set get request
+	$res = webkitd_get($fd);
+	if($res == false){
+		webkitd_close($fd);
+		die('Error: webkitd couldn\'t set http get'."\n");
+	}
+
+	//run request
+	$res = webkitd_execute($fd);
+	if($res == false){
+		echo ('Error: webkitd couldn\'t execute - errcode: '.$globalerrcode.' errstr: '.$globalerrstr."\n");
+	}
+
+	//return url might not be the one we set due to 301s/302s etc
+	$url = webkitd_returnurl($fd);
+	if($url == false){
+		webkitd_close($fd);
+		die('Error: webkitd couldn\'t return url'."\n");
+	}
+	echo "url: ".$url."\n";
+
+	/*
+	//broke as returns last header (i.e. could be an image)
+	$header= webkitd_returnheader($fd);
+	if($header == false){
+		webkitd_close($fd);
+		die('Error: webkitd couldn\'t return header'."\n");
+	}
+	echo "header: ".$header."\n";
+	*/
+
+	//run jquery, NOTE use _jQuery not _$
+	$res = webkitd_runjs($fd, "_jQuery('#test').html('test text js1');");
+
+	//returns tag soup
+	$html = webkitd_gethtml($fd);
+	if($html == false){
+		webkitd_close($fd);
+		die('Error: webkitd couldn\'t get html'."\n");
+	}
+	echo "HTML: ".$html."\n";
+
+	//return image based on regex
+	$regex = '';
+	$imgdata = webkitd_returnimage($fd, $regex);
+	if($imgdata == false){
+		webkitd_close($fd);
+		die('Error: webkitd couldn\'t return image'."\n");
+	}
+	else {
+		$ifd = fopen('./img.jpg','w');
+		if($ifd){ 
+			fwrite($ifd,$imgdata);
+			fclose($ifd);
+			echo ('wrote image'."\n");
+		}
+		else {
+			echo ('didn\'t write image'."\n");
+		}
+	}
+
+	$res = webkitd_runjquery($fd, "_jQuery('#test').html('test text js 2');");
+	$html = webkitd_gethtml($fd);
+	if($html == false){
+		webkitd_close($fd);
+		die('Error: webkitd couldn\'t get html'."\n");
+	}
+	echo "HTML: ".$html."\n";
+
+	/*
+	$res = webkitd_inputfill("input[name=enit]", "hola");
+	$res = webkitd_inputcheck("input[name=enit]", "hola");
+	$res = webkitd_inputuncheck("input[name=enit]", "hola");
+	$res = webkitd_inputchoose("input[name=enit]", "hola");
+	$res = webkitd_inputselect("input[name=enit]", "hola");
+	$res = webkitd_formsubmit("input[name=enit]", "hola");
+	*/
+
+	//close the connection to be nice to the server
+	$res = webkitd_close($fd);
+	if($res == false){
+		die('Error: webkitd couldn\'t close'."\n");
+	}
 }
 ?>
